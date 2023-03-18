e8b68d9f53672a4d87a430c97ac994e3814611d0
Emil Enchev <emoen4ev@gmail.com>
2023-03-19 01:16:08 +0200

    Added solutions part to 'File input/output'.

--- Introduction to Python/File input output/Read all lines/read_all_lines.py
+++ Introduction to Python/File input output/Read all lines/read_all_lines.py
@@ -1,5 +1,5 @@
 with open("input.txt", "r") as infile:
-    # TODO: read all lines from input.txt into the list called lines_list
+    lines_list = infile.readlines()
 
 if __name__ == "__main__":
     print(lines_list)
